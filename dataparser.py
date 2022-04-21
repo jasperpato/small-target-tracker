9c7cd9688579e297e86d5fb7d19e1e60b571f345
Allen <22706998@student.uwa.edu.au>
2022-04-22 01:55:11 +0800

    feat: adjust dataloader so it is robust to missing frames

--- dataparser.py
+++ dataparser.py
@@ -1,4 +1,6 @@
 from glob import glob
+from pathlib import Path
+import os
 import cv2
 import psutil
 import regex as re
@@ -6,30 +8,33 @@ import pandas as pd
 
 
 class Dataloader(object):
-    def __init__(self, folder_path, img_file_pattern='*.jpg', gt_file_pattern="gt.txt", frame_range=None):        
+    def __init__(self, folder_path, img_file_pattern='*.jpg', frame_range=None):        
         img_paths = glob(f'{folder_path}/**/{img_file_pattern}', recursive=True)
-        gt_file = glob(f'{folder_path}/**/{gt_file_pattern}', recursive=True)[0]
+        gt_file = glob(f'{folder_path}/**/gt.txt', recursive=True)[0]
         
-        get_frame = lambda p: int(re.search('\d+', p).group())
+        get_frame = lambda p: int(re.search('\d+', Path(p).stem).group())
         self.frame_paths = {get_frame(p): p for p in img_paths}
-        frames = self.frame_paths.keys()
         
-        self.frame_range = frame_range if frame_range is not None else (min(frames), max(frames))
-        self.gt_data = pd.read_csv(gt_file, sep=',', names=['frame', 'track_id', 'tl_x', 'tl_y', 'width', 'height'])
+        full_frame_range = self.get_full_frame_range()
+        self.frame_range = frame_range or full_frame_range
+        if self.frame_range[0] < full_frame_range[0] or self.frame_range[1] > full_frame_range[1]:
+            raise ValueError('Frame range {} is out of range {}'.format(frame_range, full_frame_range))
+        
+        self.gt_data = pd.read_csv(gt_file, sep=',', usecols=range(6), names=['frame', 'track_id', 'tl_x', 'tl_y', 'width', 'height'])
         self.preloaded_frames = {}
         self.preload_frames()
         
 
     def preload_frames(self):
         start_frame, end_frame = self.frame_range
-        loaded_frames = self.loaded_frames.keys()
-        frame_no = max(loaded_frames) + 1 if loaded_frames else start_frame
+        frame_no = max(self.preloaded_frames.keys()) + 1 if self.preloaded_frames.keys() else start_frame
         
         while psutil.virtual_memory().percent > 90 and frame_no < end_frame:
-            img = cv2.imread(self.frame_paths[frame_no], cv2.IMREAD_UNCHANGED)
-            gt_data = self.gt_data[self.gt_data.frame.eq(frame_no)]
-            gt_data = gt_data[['tl_x', 'tl_y', 'width', 'height']]  
-            self.loaded_frames[frame_no] = (frame_no, img, gt_data.to_numpy())
+            if frame_no in self.frame_paths:
+                img = cv2.imread(self.frame_paths[frame_no], cv2.IMREAD_UNCHANGED)
+                gt_data = self.gt_data[self.gt_data.frame.eq(frame_no)]
+                gt_data = gt_data[['tl_x', 'tl_y', 'width', 'height']]  
+                self.loaded_frames[frame_no] = (frame_no, img, gt_data.to_numpy())
             frame_no += 1
     
     
@@ -39,27 +44,36 @@ class Dataloader(object):
       
 
     def __call__(self, frame_no):
-        if frame_no in self.loaded_frames:
-            return self.loaded_frames.pop(frame_no)
-        else:
+        if frame_no in self.preloaded_frames:
+            return self.preloaded_frames.pop(frame_no)
+        elif frame_no in self.frame_paths:
             img = cv2.imread(self.frame_paths[frame_no], cv2.IMREAD_UNCHANGED)
             gt_data = self.gt_data[self.gt_data.frame.eq(frame_no)]
             gt_data = gt_data[['tl_x', 'tl_y', 'width', 'height']]  
             return (frame_no, img, gt_data.to_numpy())
+        else:
+            raise ValueError('Frame {} could not be found'.format(frame_no))
         
     
     def __iter__(self):
-        self.current_frame = self.frame_range[0]
+        frames = [f for f in self.frame_paths.keys() 
+                  if f >= self.frame_range[0] and f <= self.frame_range[1]]
+        self.frame_iterator = iter(sorted(frames))
         return self
             
 
     def __next__(self):
-        if self.current_frame > self.frame_range[1]:
-            raise StopIteration
-        else:
-            data = self.loaded_frames.pop(self.current_frame, None) or self.__call__(self.current_frame)
-            self.current_frame += 1
-            return data
+        frame_no = next(self.frame_iterator)
+        return self.preloaded_frames.pop(frame_no, None) or self.__call__(frame_no)
+        
+    
+if __name__ == '__main__':
+    # TESTING
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(current_dir)
+    dataloader = Dataloader('../VISO/mot/car/001', img_file_pattern='*.jpg', frame_range=(1, 100))
+    for frame_no, img, data in dataloader:
+        print(frame_no, img.shape, data.shape)
     
     
     
