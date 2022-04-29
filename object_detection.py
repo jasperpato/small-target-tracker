be1933d24f2a82e0ac28a5763c10e39ba423639d
Jasper Paterson <jasperpato@gmail.com>
2022-04-29 22:49:00 +0800

    region growing is wrong, unsure about clustering

--- object_detection.py
+++ object_detection.py
@@ -6,6 +6,7 @@ from scipy.stats import norm
 from dataparser import Dataloader
 import matplotlib.pyplot as plt
 from skimage import measure
+import cv2
 
 
 def frame2gray(frame):
@@ -32,58 +33,81 @@ def objects(grays):
   # mask
   b1, b2 = dif1 > th1, dif2 > th2
   # intersection
-  return np.logical_and(b1, b2)
+  return np.array(np.logical_and(b1, b2), dtype=np.uint8)
+
 
 def region_growing(gray, binary):
   '''
   Input: a grayscale image and corresponding binary image containing moving candidate pixels
   Output: new binary image with region growing applied
-
-  This function currently applies region growing to every candidate pixel in binary,
-  but it should apply it only to th centroid of candidate clusters. Unsure how to find centroids yet.
   '''
+
   bin = deepcopy(binary)
 
+  # label clusters in binary image
+
+  clusters = measure.label(binary)
+  num_clusters = np.max(clusters)
+
+  # sort pixels into cluster bins
+
+  cluster_points = [[] for n in range(num_clusters)]
+  for i in range(len(clusters)):
+    for j in range(len(clusters[i])):
+      num = clusters[i][j]
+      if num: cluster_points[num-1].append((i,j))
+
+  # get centroid of each cluster
+
+  centroids = []
+  for c in cluster_points:
+    my_sum_x = sum([p[0] for p in c])
+    my_sum_y = sum([p[1] for p in c])
+
+    # ignore clusters containing two pixels or less
+    # if len(c) > 2:
+
+    centroids.append((round(my_sum_x / len(c)), round(my_sum_y / len(c))))
+
   # for each pixel
-  for i in range(len(gray)):
-    for j in range(len(gray[i])):
-      if binary[i][j]:
+  for (i, j) in centroids:
 
-        # calculate mean
-        sum, num = 0, 0
-        # for each surround pixel in 11x11 box
-        for p in range(i-5, i+6):
-          for q in range(j-5, j+6):
-            # if pixel is inside image bounds
-            if not (p < 0 or p >= len(gray) or q < 0 or q >= len(gray[i])): 
-              sum += gray[p][q]
-              num += 1
-        mean = sum / num
+      # calculate mean
+      my_sum, num = 0, 0
 
-        # calculate SD
-        sum, num = 0, 0
-        for p in range(i-5, i+6):
-          for q in range(j-5, j+6):
-            if not (p < 0 or p >= len(gray) or q < 0 or q >= len(gray[i])): 
-              sum += (gray[p][q] - mean) ** 2
-              num += 1
-        sd = (sum / num) ** 0.5
+      # for each surround pixel in 11x11 box
+      for p in range(i-5, i+6):
+        for q in range(j-5, j+6):
 
-        # get thresholds from ppf
-        t1, t2 = 0, 0
-        if sd:
-          ppf = norm.ppf(0.95, loc=mean, scale=sd)
-          t1, t2 = (2 * mean - ppf, ppf)
-        else: t1, t2 = mean, mean
+          # if pixel is inside image bounds
+          if not (p < 0 or p >= len(gray) or q < 0 or q >= len(gray[i])): 
+            my_sum += gray[p][q]
+            num += 1
+      mean = my_sum / num
 
-        # apply thresholding to grow regions in binary image
-        for p in range(i-5, i+6):
-          for q in range(j-5, j+6):
-            if not (p < 0 or p >= len(gray) or q < 0 or q >= len(gray[i])):
-              # if grey level pixel is in quantile, set to 1
-              if gray[p][q] >= t1 and gray[p][q] <= t2: bin[p][q] = True 
+      # calculate SD
+      my_sum, num = 0, 0
+      for p in range(i-5, i+6):
+        for q in range(j-5, j+6):
+          if not (p < 0 or p >= len(gray) or q < 0 or q >= len(gray[i])): 
+            my_sum += (gray[p][q] - mean) ** 2
+            num += 1
+      sd = (my_sum / num) ** 0.5
 
-      # if not i % 100 and not j % 100: print(i,j) 
+      # get thresholds from ppf
+      t1, t2 = 0, 0
+      if sd:
+        ppf = norm.ppf(0.95, loc=mean, scale=sd)
+        t1, t2 = (2 * mean - ppf, ppf)
+      else: t1, t2 = mean, mean
+
+      # apply thresholding to grow regions in binary image
+      for p in range(i-5, i+6):
+        for q in range(j-5, j+6):
+          if not (p < 0 or p >= len(gray) or q < 0 or q >= len(gray[i])):
+
+            # if grey level pixel is in quantile, set to 1
+            if gray[p][q] >= t1 and gray[p][q] <= t2: bin[p][q] = True
 
   return bin
 
