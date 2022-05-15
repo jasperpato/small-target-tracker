e6ff823b902187539fc7b954fbe26d9d40c14cde
Allen <22706998@student.uwa.edu.au>
2022-05-15 10:08:31 +0800

    Improve readability by adding Box class

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,49 @@
 import numpy as np
 
+class Box:
+    def __init__(self, xtl, ytl, w, h):
+        self.xtl = xtl
+        self.ytl = ytl
+        self.xbr = w + xtl
+        self.ybr = h + ytl
+        self.w = w
+        self.h = h
+    
+    @property    
+    def area(self):
+        return (self.xbr - self.xtl) * (self.ybr - self.ytl)
+    
+    
+def is_intersecting(cls, box1: 'Box', box2: 'Box'):
+    if box1.xtl > box2.xbr:
+        return False  # box1 is right of box2
+    if box2.xtl > box1.xbr:
+        return False  # box1 is left of box2
+    if box1.ybr < box2.ytl:
+        return False  # box1 is above box2
+    if box1.ytl > box2.ybr:
+        return False  # box1 is below box2
+    return True
 
+
+def intersection_over_union(cls, box1: 'Box', box2: 'Box'):
+    """
+    Calculate the intersection over union (IOU) of two boxes.
+    Boxes are in the format [tl_x, tl_y, width, height]
+    """
+    xtl = max(box1.xtl, box2.xtl)
+    ytl = max(box1.ytl, box2.ytl)
+    xbr = min(box1.xbr, box2.xbr)
+    ybr = min(box1.ybr, box2.ybr)
+    
+    if not Box.is_intersecting(box1, box2):
+        return 0.0
+
+    intersection = (xbr - xtl) * (ybr - ytl)
+    union = box1.area + box2.area - intersection
+    return max(intersection / union, 0)
+    
+    
 def evaluation_metrics(pred_bboxes, gt_bboxes, iou_threshold=0.7):
     """ 
     Compute evaluation metrics for a set of predicted and ground truth bounding boxes.
@@ -20,7 +63,7 @@ def evaluation_metrics(pred_bboxes, gt_bboxes, iou_threshold=0.7):
     for pred in pred_bboxes:
         max_iou = 0.0
         for i in range(npos):
-            iou = intersection_over_union(pred, gt_bboxes[i])
+            iou = Box.intersection_over_union(pred, gt_bboxes[i])
             if iou > max_iou:
                 max_iou = iou
                 gt_idx = i  # index of the ground truth box with the highest IoU
@@ -39,39 +82,3 @@ def evaluation_metrics(pred_bboxes, gt_bboxes, iou_threshold=0.7):
     recall = tp / npos
     f1 = 2 * precision * recall / (precision + recall)
     return {'precision': precision, 'recall': tp / npos, 'f1': f1}
-    
-
-def is_intersecting(box1, box2):
-    """
-    Check if two boxes are intersecting. Boxes are in the format
-    [tl_x, tl_y, width, height]
-    """
-    
-    if box1[0] > box2[0] + box2[2]:
-        return False  # box1 is right of box2
-    if box2[0] > box1[0] + box1[2]:
-        return False  # box2 is right of box1
-    if box1[1] > box2[1] + box2[3]:
-        return False  # box1 is below box2
-    if box2[1] > box1[1] + box1[3]:
-        return False  # box2 is below box1
-    return True
-
-
-def intersection_over_union(box1, box2):
-    """
-    Calculate the intersection over union (IOU) of two boxes.
-    Boxes are in the format [tl_x, tl_y, width, height]
-    """
-    
-    xtl = max(box1[0], box2[0])
-    ytl = max(box1[1], box2[1])
-    xbr = min(box1[0] + box1[2], box2[0] + box2[2])
-    ybr = min(box1[1] + box1[3], box2[1] + box2[3])
-    
-    if not is_intersecting(box1, box2):
-        return 0.0
-
-    intersection = (xbr - xtl) * (ybr - ytl)
-    union = box1[2] * box1[3] + box2[2] * box2[3] - intersection
-    return max(intersection / union, 0)
