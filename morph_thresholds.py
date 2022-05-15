500e359fa0cc79ee0609eda9d61d4bc2ecc53d9d
Allen <22706998@student.uwa.edu.au>
2022-05-15 22:54:41 +0800

    improve bounding box visualisation

--- morph_thresholds.py
+++ morph_thresholds.py
@@ -12,7 +12,7 @@ from object_detection import objects, region_growing
 from evaluation import intersection_over_union, Box
 
 
-def plot_morph_cues(binary, gt_boxes, ax, iou_threshold=0.5):
+def plot_morph_cues(binary, gt_boxes, ax, iou_threshold=0.4):
   '''
   Plot morphological features for true positives and false positives
   gt list of ground truths bounding box data
@@ -20,7 +20,7 @@ def plot_morph_cues(binary, gt_boxes, ax, iou_threshold=0.5):
   binary = deepcopy(binary)
   labeled_image = measure.label(binary, background=0, connectivity=1)
   blobs = measure.regionprops(labeled_image)
-  
+
   npos = len(gt_boxes)
   seen_gts = np.zeros(npos)
   tp_areas = []
@@ -29,22 +29,27 @@ def plot_morph_cues(binary, gt_boxes, ax, iou_threshold=0.5):
   tp_eccentricities = []
 
   for blob in blobs:
-    bbox = blob.bbox
-    
+    pred_box = Box(xtl=blob.bbox[1], ytl=blob.bbox[0],
+                   w=blob.bbox[3] - blob.bbox[1],
+                   h=blob.bbox[2] - blob.bbox[0])
     max_iou = 0.0
     for i in range(npos):
-      iou = intersection_over_union(Box(bbox), Box(gt_boxes[i]))
+      gt_box = Box(*gt_boxes[i])
+      iou = intersection_over_union(pred_box, gt_box)
       if iou > max_iou:
         max_iou = iou
         gt_idx = i  # index of the ground truth box with the highest IoU
-    
+
     if max_iou >= iou_threshold and seen_gts[gt_idx] == 0:
       seen_gts[gt_idx] = 1    # mark as detected
       tp_areas.append(blob.area_filled)
       tp_extents.append(blob.area_filled / blob.area_bbox)
       tp_a_lengths.append(blob.axis_major_length)
       tp_eccentricities.append(blob.eccentricity)
-      ax.add_patch(patches.Rectangle((bbox[1], bbox[0]), bbox[3]-bbox[1], bbox[2]-bbox[0], linewidth=1, edgecolor='b', facecolor='none'))
+      ax.add_patch(patches.Rectangle((pred_box.xtl - 0.5, pred_box.ytl - 0.5),
+                                     pred_box.xbr - pred_box.xtl + 1,
+                                     pred_box.ybr - pred_box.ytl + 1,
+                                     linewidth=1, edgecolor='g', facecolor='none'))
 
 
 if __name__ == '__main__':
@@ -52,18 +57,21 @@ if __name__ == '__main__':
   dataset_path = sys.argv[1].rstrip('/')
   dataloader = Dataloader(f'{dataset_path}/car/001', img_file_pattern='*.jpg', frame_range=(1, 100))
   frames = list(dataloader.preloaded_frames.values())
-  i0 = 5
-  
+  i0 = 10
+
   imgs = (frames[0], frames[i0], frames[2 * i0])
   grays = [color.rgb2gray(i[1]) for i in imgs]
-    
+
   binary = objects(grays)
   grown = region_growing(grays[1], binary)
 
   fig, ax = plt.subplots()
   ax.imshow(grown, cmap='gray')
-
-  plot_morph_cues(grown, frames[i0][2], ax)
+  
   for box in imgs[1][2]:
-    ax.add_patch(patches.Rectangle((box[0], box[1]), box[2], box[3], linewidth=1, edgecolor='r', facecolor='none'))
+    ax.add_patch(patches.Rectangle((box[0] - 0.5, box[1] - 0.5),
+                                   box[2], box[3], linewidth=1,
+                                   edgecolor='y', facecolor='none'))
+
+  plot_morph_cues(grown, imgs[1][2], ax)
   plt.show(block=True)
